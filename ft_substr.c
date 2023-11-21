c98ca7803554d518826830d07e04a302220660c2
とこち <34222155+tokochiz@users.noreply.github.com>
2023-11-21 22:36:41 +0900

    Add files via upload

--- ft_substr.c
+++ ft_substr.c
@@ -3,19 +3,19 @@
 /*                                                        :::      ::::::::   */
 /*   ft_substr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
+/*   By: ctokoyod <ctokoyod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 22:00:36 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2023/09/19 22:32:39 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2023/11/21 22:34:44 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-	
+    // 家からcommit test
+    // test
 }
 
 int main(void)
 {
-	
 }
