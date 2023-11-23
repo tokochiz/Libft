e2f291d2bda665fd2dd44a8e063247302a446115
tokochiz <tokyd1003@gmail.com>
2023-11-23 23:58:55 +0900

    1123 commit

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,14 @@
 /*   By: ctokoyod <ctokoyod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 22:00:36 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2023/11/21 22:25:52 by ctokoyod         ###   ########.fr       */
+/*   Updated: 2023/11/23 23:58:39 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
- // 家からcommit test
+    // 家からcommit test
+    // 20231123
 }
 
 int main(void)
