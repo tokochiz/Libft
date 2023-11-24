6b33dc04c62bd5555e0bdb6baf057e95457f4a5f
tokochiz <tokyd1003@gmail.com>
2023-11-24 23:57:08 +0900

    error 修正

--- ft_substr.c
+++ ft_substr.c
@@ -6,13 +6,13 @@
 /*   By: ctokoyod <ctokoyod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 22:00:36 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2023/11/24 23:41:13 by ctokoyod         ###   ########.fr       */
+/*   Updated: 2023/11/24 23:54:55 by ctokoyod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char *ft_substr(char const *s, unsigned int start, size_t len)
 {
-    
+    testtesttest
     
 }
 
