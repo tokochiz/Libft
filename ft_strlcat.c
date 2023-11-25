cd96364d144fad38c689f53ee56230e93ada48ca
tokochiz <tokohana.1003@gmail.com>
2023-11-25 23:37:28 +0900

    first commit

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,11 +6,13 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/25 21:14:31 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2023/11/25 21:14:43 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2023/11/25 23:37:18 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 size_t ft_strlcat(char * dst, const char * src, size_t dstsize)
 {
+	
+
 	
 }
