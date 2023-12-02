53f1a95260805067a1a961d29347f2146f9f3d9b
tokochiz <tokohana.1003@gmail.com>
2023-12-02 17:56:55 +0900

    first commit

---
+++ ft_strrchr.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrchr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/02 17:43:53 by  ctokoyod         #+#    #+#             */
+/*   Updated: 2023/12/02 17:56:22 by  ctokoyod        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char *ft_strrchr(const char *s, int c)
+{
+	const char *tmp;
+	const char *last;
+    tmp = s;
+	last = NULL;
+    while (*tmp != '\0')
+    {
+        if (*tmp == c)
+			last = tmp;
+        tmp++;
+    }
+    return (char *)last;
+
+}
+int main(void)
+{
+    char test[20] = "abcdebg";
+    char *p;
+
+    p = ft_strrchr(test, 'b');
+    if (p != NULL)
+    {
+        printf("ans:%s\n", p);
+    }
+    else
+    {
+        printf("not found\n");
+    }
+
+    return 0;
+}
+
