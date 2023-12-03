7a3f74e67417f09513f4485d5437e0bdd85dbfec
tokochiz <tokohana.1003@gmail.com>
2023-12-03 16:19:17 +0900

    long max/min 未対応

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,22 +6,53 @@
 /*   By:  ctokoyod < ctokoyod@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 22:42:33 by  ctokoyod         #+#    #+#             */
-/*   Updated: 2023/12/02 22:54:02 by  ctokoyod        ###   ########.fr       */
+/*   Updated: 2023/12/03 16:16:15 by  ctokoyod        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+int	ft_isspace(const char *str)
+{
+	if (*str == ' ' || *str == '\f' || *str == '\n' || *str == '\r'
+		|| *str == '\t' || *str == '\v')
+		return (1);
+	return (0);
+}
+
 int	ft_atoi(const char *str)
 {
-	const unsigned char *tmp;
-	tmp = *str;
-	int ans;
-	while(tmp != '\0')
+	int			sign;
+	long		result;
+
+	sign = 1;
+	result = 0;
+	while (*str != '\0')
 	{
-		if(tmp == '-')
-			ans = ans * -1;
-	
+		while (ft_isspace(str))
+			str++;
+		while (*str == '-' || *str == '+')
+		{
+			if (*str == '-')
+				sign = sign * -1;
+			str++;
+			while (*str >= '0' && *str <= '9')
+			{
+				result = result * 10 + (*str - '0');
+				str++;
+			}
+		}
+		return (int)(sign * result);
 	}
-	
+	return 0;
+}
+
+int	main(void)
+{
+	const char *str = "  -12345abc";
+	int result = ft_atoi(str);
+
+	printf("Result: %d\n", result);
+
+	return (0);
 }
